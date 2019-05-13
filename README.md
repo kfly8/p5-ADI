ab8ee87d21914424b153dedeea0c9831aafeff2e
kfly8 <kfly@cpan.org>
2019-05-14 07:39:45 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+ADI - It's new $module
+
+# SYNOPSIS
+
+    use ADI;
+
+# DESCRIPTION
+
+ADI is ...
+
+# LICENSE
+
+Copyright (C) kfly8.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kfly8 <kfly@cpan.org>
---
+++ lib/ADI.pm
@@ -0,0 +1,39 @@
+package ADI;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+ADI - It's new $module
+
+=head1 SYNOPSIS
+
+    use ADI;
+
+=head1 DESCRIPTION
+
+ADI is ...
+
+=head1 LICENSE
+
+Copyright (C) kfly8.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kfly8 E<lt>kfly@cpan.orgE<gt>
+
+=cut
+
